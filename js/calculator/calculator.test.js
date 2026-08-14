7175bca51b0d949417ca3182c77f1535d99ae16b
Diana Cruz <dianacd1234567@gmail.com>
2026-08-14 15:27:16 -0600

    tarea 1

--- js/calculator/calculator.test.js
+++ js/calculator/calculator.test.js
@@ -15,3 +15,17 @@ test('subtract 5 - 4 to equal 1', () => {
 test('subtract -5 - -10 to equal 5', () => {
   expect(calculator.subtract(-5, -10)).toBe(5);
 });
+
+test('divide 10 to 5 equal 2', () => {
+  expect(calculator.divide(10, 5)).toBe(2);
+});
+test('divide 15 to 5 equal 3', () => {
+  expect(calculator.divide(15, 5)).toBe(3);
+});
+
+test('multiply 15 to 3 equal 45', () => {
+  expect(calculator.multiply(15, 3)).toBe(45);
+});
+test('multiply 6 to 3 equal 18', () => {
+  expect(calculator.multiply(6, 3)).toBe(18);
+});
